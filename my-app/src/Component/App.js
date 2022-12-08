b207c5f3bb38b356f3e1033dc5bf49a28afafcd4
Ehsan <ehsanellahi051@gmail.com>
2022-12-08 16:34:52 +0500

    Initial Commit

--- my-app/src/Component/App.js
+++ my-app/src/Component/App.js
@@ -1,13 +1,20 @@
-import Header from './Header';
-import Hero from './Hero';
-import Footer from './Footer';
+// import Header from './Header';
+// import Hero from './Hero';
+// import Footer from './Footer';
+import Card from './contact';
 
 function App() {
   return (
-    <div className="App">
-      <Header />
+    <div className="App" >
+      {/* <Header />
       <Hero />
-      <Footer />
+      <Footer /> */}
+      <Card 
+        name="Ehsan Ellhai"
+        img=""
+        mail="ehsanellahiofficial@gmail.com"
+        num="+923078607264"
+      />
     </div>
   );
 }
