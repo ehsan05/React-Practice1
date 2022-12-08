f1412008137d6cdc04ec969d79fd581253084a2b
Ehsan <ehsanellahi051@gmail.com>
2022-12-08 12:47:41 +0500

    Initial Commit

---
+++ my-app/src/Component/App.js
@@ -0,0 +1,15 @@
+import Header from './Header';
+import Hero from './Hero';
+import Footer from './Footer';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Hero />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
